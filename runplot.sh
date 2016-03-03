2e3c20360f344e36c7e20e1f960ed01bff38a775
kevin <kevin82093098@gmail.com>
2016-03-03 11:56:44 +0800

    test2

--- runplot.sh
+++ runplot.sh
@@ -1,3 +1,4 @@
 rm opt.txt orig.txt output.txt
+echo 1 | sudo tee /proc/sys/vm/drop_caches
 make plot
 eog runtime.png
--- runreport.sh
+++ runreport.sh
@@ -1,2 +1,3 @@
+echo 1 | sudo tee /proc/sys/vm/drop_caches
 rm opt.txt orig.txt output.txt
 perf record -e cache-misses  make plot
